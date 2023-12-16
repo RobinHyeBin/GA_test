a5e8bbabccfd75504ce07d884ab2efa3e8ebfc6c
RobinHyeBin <robin@nextunicorn.kr>
2023-12-17 00:30:55 +0900

    feat: GA 추가

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtag";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
